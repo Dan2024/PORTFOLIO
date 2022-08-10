f3cd3ccaa052015c32b6391b0aa4d33dddde04fa
Dan <dannorthcott8@gmail.com>
2022-08-10 20:04:45 +0100

    inital commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <>
+      <ul className='hidden md:grid grid-cols-5 place-items-center'>
+        <li className=''>Home</li>
+        <li className=''>About</li>
+        <li className=''>Skills</li>
+        <li className=''>Projects</li>
+        <li className=''>Contact</li>
+      </ul>
+    </>
+  )
+}
