6c21c75da48420a07971e25596c0cca2decbca06
Dan <dannorthcott8@gmail.com>
2022-08-18 21:10:00 +0100

    addition of a portfolio image

--- components/MyWork.jsx
+++ components/MyWork.jsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+
 export default function MyWork() {
   return (
     <section className='max-w-7xl mx-auto'>
-      My Work
-      <div></div>
+      <h3>My Work</h3>
+      <div className=''>
+        {/* <Image src='/codified.png' layout='fill' width={500} height={500} /> */}
+        <Image
+          src='/codified.png'
+          width='900px'
+          height='700px'
+          objectFit='cover'
+        />
+      </div>
     </section>
   )
 }
