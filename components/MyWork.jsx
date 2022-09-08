5ac2d6a68725a6ce8a6ddbee1c0e3f88b095c5b8
Dan <dannorthcott8@gmail.com>
2022-09-08 22:03:20 +0100

    optimised GLTFs files with GLTFJSX

--- components/MyWork.jsx
+++ components/MyWork.jsx
@@ -1,6 +1,7 @@
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { useFrame, Canvas, useThree } from '@react-three/fiber'
+import { useFrame, Canvas } from '@react-three/fiber'
 import { useEffect, useRef, Suspense, useState } from 'react'
+import { useGLTF } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const angleToRadians = (angleInDeg) => (Math.PI / 180) * angleInDeg
@@ -8,10 +9,11 @@ const angleToRadians = (angleInDeg) => (Math.PI / 180) * angleInDeg
 function Scene() {
   // Code to move the camera around
   const orbitControlsRef = useRef(null)
+
   useFrame((state) => {
-    orbitControlsRef.current.dampingFactor = 0.003
-    orbitControlsRef.current.enableZoom = false
     if (!!orbitControlsRef.current) {
+      orbitControlsRef.current.dampingFactor = 0.003
+      orbitControlsRef.current.enableZoom = false
       const { x, y } = state.mouse
       // console.log(orbitControlsRef.current)
       orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(30))
@@ -20,16 +22,32 @@ function Scene() {
     }
   })
 
-  const PortfolioMonitor = () => {
-    const [model, setModel] = useState()
-
-    useEffect(() => {
-      new GLTFLoader().load('/portfolio-monitor-3.glb', setModel)
-    })
-
-    return model ? (
-      <primitive castShadow object={model.scene} position={[0, 0, 0]} />
-    ) : null
+  const PortfolioMonitor = (props) => {
+    const { nodes, materials } = useGLTF('/portfolio-monitor-3-transformed.glb')
+    return (
+      <group {...props} dispose={null}>
+        <mesh
+          geometry={nodes.Object.geometry}
+          material={materials['Material.001']}
+          position={[0, -2, -0.9]}
+          rotation={[0, 1.57, 0]}
+        />
+        <mesh
+          geometry={nodes['Screenshot_2022-09-08_at_100154'].geometry}
+          material={materials['Screenshot 2022-09-08 at 10.01.54']}
+          position={[0, 1, 0.1]}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={4.06}
+        />
+      </group>
+    )
+    // const [model, setModel] = useState()
+    // useEffect(() => {
+    //   new GLTFLoader().load('/portfolio-monitor-3.glb', setModel)
+    // })
+    // return model ? (
+    //   <primitive castShadow object={model.scene} position={[0, 0, 0]} />
+    // ) : null
   }
 
   const Floor = () => {
@@ -51,8 +69,8 @@ function Scene() {
       <PerspectiveCamera makeDefault position={[0, 3, 9]} />
       <OrbitControls
         ref={orbitControlsRef}
-        minPolarAngle={angleToRadians(60)}
-        maxPolarAngle={angleToRadians(85)}
+        minPolarAngle={angleToRadians(70)}
+        maxPolarAngle={angleToRadians(90)}
         enableDampening={true}
         panSpeed={0.1}
       />
@@ -65,7 +83,7 @@ function Scene() {
 
       {/* Objects */}
       <PortfolioMonitor />
-      <Floor />
+      {/* <Floor /> */}
 
       {/* Lighting */}
       <ambientLight args={['#ffffff', 0.25]} />
@@ -82,8 +100,18 @@ function Scene() {
 export default function MyWork() {
   return (
     <div className='w-[500px] h-[400px] shadow-xl ml-20'>
-      <Canvas frameloop='demand' shadows>
-        <Suspense fallback={<></>}>
+      <Canvas shadows>
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+      </Canvas>
+      <Canvas shadows>
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+      </Canvas>
+      <Canvas shadows>
+        <Suspense fallback={null}>
           <Scene />
         </Suspense>
       </Canvas>
