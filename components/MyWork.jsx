8b8b49374f8d346750d4992d33fae985dff12655
Dan <dannorthcott8@gmail.com>
2022-09-09 09:17:57 +0100

    portfolio monitor component made reusable

--- components/MyWork.jsx
+++ components/MyWork.jsx
@@ -6,26 +6,39 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const angleToRadians = (angleInDeg) => (Math.PI / 180) * angleInDeg
 
-function Scene() {
+const codifeyedMonitorGLTFPath = '/glb/codifeyed-monitor-transformed.glb'
+const codifeyedMonitorImgNodeName = 'Screenshot_2022-09-08_at_100114'
+const codifeyedMonitorImgMaterialName = 'Screenshot 2022-09-08 at 10.01.14'
+
+const portfolioMonitorGLTFPath = '/glb/portfolio-monitor-3-transformed.glb'
+const portfolioMonitorImgNodeName = 'Screenshot_2022-09-08_at_100154'
+const portfolioMonitorImgMaterialName = 'Screenshot 2022-09-08 at 10.01.54'
+
+function Scene({
+  monitorGLTFPath,
+  monitorImgNodeName,
+  monitorImgMaterialName,
+}) {
   // Code to move the camera around
   const orbitControlsRef = useRef(null)
 
   useFrame((state) => {
     if (!!orbitControlsRef.current) {
+      const { x, y } = state.mouse
+
       orbitControlsRef.current.dampingFactor = 0.003
       orbitControlsRef.current.enableZoom = false
-      const { x, y } = state.mouse
-      // console.log(orbitControlsRef.current)
+
       orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(30))
       orbitControlsRef.current.setPolarAngle((y + 1) * angleToRadians(90 - 30))
       orbitControlsRef.current.update()
     }
   })
 
-  const PortfolioMonitor = (props) => {
-    const { nodes, materials } = useGLTF('/portfolio-monitor-3-transformed.glb')
+  const Monitor = () => {
+    const { nodes, materials } = useGLTF(monitorGLTFPath)
     return (
-      <group {...props} dispose={null}>
+      <group dispose={null}>
         <mesh
           geometry={nodes.Object.geometry}
           material={materials['Material.001']}
@@ -33,33 +46,13 @@ function Scene() {
           rotation={[0, 1.57, 0]}
         />
         <mesh
-          geometry={nodes['Screenshot_2022-09-08_at_100154'].geometry}
-          material={materials['Screenshot 2022-09-08 at 10.01.54']}
+          geometry={nodes[monitorImgNodeName].geometry}
+          material={materials[monitorImgMaterialName]}
           position={[0, 1, 0.1]}
           rotation={[Math.PI / 2, 0, 0]}
           scale={4.06}
         />
       </group>
-    )
-    // const [model, setModel] = useState()
-    // useEffect(() => {
-    //   new GLTFLoader().load('/portfolio-monitor-3.glb', setModel)
-    // })
-    // return model ? (
-    //   <primitive castShadow object={model.scene} position={[0, 0, 0]} />
-    // ) : null
-  }
-
-  const Floor = () => {
-    return (
-      <mesh
-        rotation={[-angleToRadians(90), 0, 0]}
-        position={[0, -3, 0]}
-        receiveShadow
-      >
-        <planeGeometry args={[40, 20]} />
-        <meshStandardMaterial color='#58bedb' />
-      </mesh>
     )
   }
 
@@ -75,15 +68,8 @@ function Scene() {
         panSpeed={0.1}
       />
 
-      {/* Ball */}
-      {/* <mesh position={[0, 0, 0]} castShadow>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial color='#ffffff' metalness={0.6} roughness={0.2} />
-      </mesh> */}
-
-      {/* Objects */}
-      <PortfolioMonitor />
-      {/* <Floor /> */}
+      {/* Monitor Object */}
+      <Monitor />
 
       {/* Lighting */}
       <ambientLight args={['#ffffff', 0.25]} />
@@ -102,17 +88,20 @@ export default function MyWork() {
     <div className='w-[500px] h-[400px] shadow-xl ml-20'>
       <Canvas shadows>
         <Suspense fallback={null}>
-          <Scene />
+          <Scene
+            monitorGLTFPath={codifeyedMonitorGLTFPath}
+            monitorImgNodeName={codifeyedMonitorImgNodeName}
+            monitorImgMaterialName={codifeyedMonitorImgMaterialName}
+          />
         </Suspense>
       </Canvas>
       <Canvas shadows>
         <Suspense fallback={null}>
-          <Scene />
-        </Suspense>
-      </Canvas>
-      <Canvas shadows>
-        <Suspense fallback={null}>
-          <Scene />
+          <Scene
+            monitorGLTFPath={portfolioMonitorGLTFPath}
+            monitorImgNodeName={portfolioMonitorImgNodeName}
+            monitorImgMaterialName={portfolioMonitorImgMaterialName}
+          />
         </Suspense>
       </Canvas>
     </div>
